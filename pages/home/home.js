b70f843646a614480210b45051ef8e2d2ab20f47
unknown <chenjin@wlyd.local>
2019-07-30 10:21:28 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,30 @@
-// pages/home/home.js
 Page({
+  data:{
+    name:'Codewhy',
+    age:20,
+    students:[
+      { id: 110, name: '张三', age: '10'},
+      { id: 111, name: '李四', age: '20' },
+      { id: 112, name: '王五', age: '30' },
+      { id: 113, name: '刘六', age: '40' }
+    ],
+    counter: 0
+  },
+  handleBtnClick(){
+    //1.错误做法：界面是不会刷新的
+    // console.log('按钮发生了点击')
+    // this.data.counter += 1
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    //2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  handleBtnSubtraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
 
   }
 })
