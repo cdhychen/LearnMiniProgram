8d797d0d7acb75222976d3fe67586d4ef8a4651e
unknown <chenjin@wlyd.local>
2019-07-31 16:33:05 +0800

    注册小程序和页面

--- pages/home/home.js
+++ pages/home/home.js
@@ -10,6 +10,61 @@ Page({
     ],
     counter: 0
   },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log('onLoad')
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    console.log('onReady')
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    console.log('onShow')
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    console.log('onHide')
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    console.log('onUnload')
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    console.log('onPullDownRefresh')
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    console.log('onReachBottom')
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    console.log('onShareAppMessage')
+  },
   handleBtnClick(){
     //1.错误做法：界面是不会刷新的
     // console.log('按钮发生了点击')
@@ -28,3 +83,7 @@ Page({
 
   }
 })
+
+//编程范式：
+//1.命令式编程：原生操作DOM
+//2.声明式编程：vue/React/Angular
